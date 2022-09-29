7774908a657f62af7725ed8475a4f9641f2c8d5a
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-09-29 22:15:27 +0530

    error page

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,33 +1,99 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+	const { repos } = React.useContext(GithubContext);
+
+	let languages = repos.reduce((total, item) => {
+		const { language, stargazers_count } = item;
+
+		if (!language) return total;
+
+		if (total[language]) {
+			total[language] = {
+				...total[language],
+				value: total[language].value + 1,
+				stars: total[language].stars + stargazers_count,
+			};
+		} else {
+			total[language] = {
+				label: language,
+				value: 1,
+				stars: stargazers_count,
+			};
+		}
+
+		return total;
+	}, {});
+
+	const mostUsed = Object.values(languages)
+		.sort((a, b) => {
+			return b.value - a.value;
+		})
+		.slice(0, 5);
+
+	//most stars per language
+	const mostPopular = Object.values(languages)
+		.sort((a, b) => {
+			return b.stars - a.stars;
+		})
+		.map((item) => {
+			return { ...item, value: item.stars };
+		})
+		.slice(0, 5);
+
+	const chartData = [
+		{
+			label: "HTML",
+			value: "13",
+		},
+		{
+			label: "CSS",
+			value: "23",
+		},
+		{
+			label: "Javascript",
+			value: "80",
+		},
+	];
+
+	return (
+		<section className='section'>
+			<Wrapper className='section-center'>
+				{/* <ExampleChart data={chartData} />; */}
+				{<Pie3D data={mostUsed} />}
+				<div></div>
+				{<Doughnut2D data={mostPopular} />}
+				<div></div>
+			</Wrapper>
+		</section>
+	);
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  justify-items: center;
-  gap: 2rem;
-  @media (min-width: 800px) {
-    grid-template-columns: 1fr 1fr;
-  }
+	display: grid;
+	justify-items: center;
+	gap: 2rem;
+	@media (min-width: 800px) {
+		grid-template-columns: 1fr 1fr;
+	}
 
-  @media (min-width: 1200px) {
-    grid-template-columns: 2fr 3fr;
-  }
+	@media (min-width: 1200px) {
+		grid-template-columns: 2fr 3fr;
+	}
 
-  div {
-    width: 100% !important;
-  }
-  .fusioncharts-container {
-    width: 100% !important;
-  }
-  svg {
-    width: 100% !important;
-    border-radius: var(--radius) !important;
-  }
+	div {
+		width: 100% !important;
+	}
+	.fusioncharts-container {
+		width: 100% !important;
+	}
+	svg {
+		width: 100% !important;
+		border-radius: var(--radius) !important;
+	}
 `;
 
 export default Repos;
