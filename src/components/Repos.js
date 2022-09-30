f4b73f41194d08f1da6caf0a5165ea31297f328e
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-09-30 22:47:40 +0530

    column-bar chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -64,9 +64,9 @@ const Repos = () => {
 			<Wrapper className='section-center'>
 				{/* <ExampleChart data={chartData} />; */}
 				{<Pie3D data={mostUsed} />}
-				<div></div>
+				{<Column3D data={chartData} />}
 				{<Doughnut2D data={mostPopular} />}
-				<div></div>
+				{<Bar3D data={chartData} />}
 			</Wrapper>
 		</section>
 	);
